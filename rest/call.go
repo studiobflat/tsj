d4fbcec9e42bf43b79e2959c504c2841ccf2394c
HaoLe <hao.le@tesuji.io>
2024-08-08 12:21:06 +0700

    chore: add new wrapper

--- rest/call.go
+++ rest/call.go
@@ -21,3 +21,20 @@ func Call[REQ any](e vndcontext.VndContext, req *REQ, name string, delegate Call
 
 	return delegate(log, e, req)
 }
+
+type CallDelegateAny[REQ any] func(*logger.Logger, vndcontext.VndContext, *REQ) (any, error)
+
+func CallAny[REQ any](e vndcontext.VndContext, req *REQ, name string, delegate CallDelegateAny[REQ]) (any, error) {
+	log := logger.GetLogger(name)
+	defer func() {
+		log.Infow("completed")
+		log.Sync()
+	}()
+
+	requestId := e.RequestId()
+	log.With([]interface{}{
+		"request_id", requestId,
+	}...)
+
+	return delegate(log, e, req)
+}
